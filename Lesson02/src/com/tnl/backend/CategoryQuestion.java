5d84d0961656a1a3c87fc19f8cda075e027a7945
Yin <132822814+LingAllie@users.noreply.github.com>
2024-07-19 23:56:11 +0700

    done_create

---
+++ Lesson02/src/com/tnl/backend/CategoryQuestion.java
@@ -0,0 +1,32 @@
+package com.tnl.backend;
+
+public class CategoryQuestion {
+	int categoryId;
+	String categoryName;
+	
+	public CategoryQuestion () {}
+	
+	public CategoryQuestion(int categoryId, String categoryName) {
+		
+		this.categoryId = categoryId;
+		this.categoryName = categoryName;
+	}
+
+	public int getCategoryId() {
+		return categoryId;
+	}
+
+	public void setCategoryId(int categoryId) {
+		this.categoryId = categoryId;
+	}
+
+	public String getCategoryName() {
+		return categoryName;
+	}
+
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+	
+	
+}
---
+++ Lesson02/src/com/tnl/backend/Main.java
@@ -0,0 +1,9 @@
+package com.tnl.backend;
+
+public class Main {
+	
+	public static void main (String[] args) {
+		
+		DataInitialization.InitializeData();
+	}
+}
